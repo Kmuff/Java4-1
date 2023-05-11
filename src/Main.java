a4e86f1a577503f50646fb2bc43fba27c06eea87
Neto test <munkey@list.ru>
2023-05-11 13:29:22 +0300

    Origin master

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int price = 12_600;
-        int miles = service.calculate(price); // должно получиться 500
+        int miles = service.calculate(price); // должно получиться 630
         System.out.println(miles);
     }
 
